e82e0fffd332bdda2b4d8b185554730a329d2be1
Lucas Goulart <goulartlcs@gmail.com>
2021-09-25 18:38:48 -0300

    Criaçao da rota /cadastrar para cadastrar um usuario

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,12 +5,11 @@ const yup = require("yup");
 const registerUser = async (req, res) => {
   const schema = yup.object().shape({
     name: yup.string().required(),
+    
   });
 
   try {
-    await schema.validate({
-      
-    });
+    await schema.validate(req.body);
   } catch (error) {
     res.status(400).json(error.message);
   }
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const routers = express();
+
+const users = require('../controllers/users');
+
+routers.post('/cadastrar', users.registerUser);
 
 
 module.exports = routers;
