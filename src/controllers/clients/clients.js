af2be5330c5118b840ac569f6027671023a3fc36
Lucas Goulart <goulartlcs@gmail.com>
2021-09-30 22:31:35 -0300

    Correçao de bug cadastro de cliente

--- src/controllers/clients/clients.js
+++ src/controllers/clients/clients.js
@@ -5,26 +5,26 @@ const jwt = require("jsonwebtoken");
 const clientRegistration = async (req, res) => {
   const { name, email, cpf, state,
     phone, zipcode, street, district,
-    city, additional, landmark,
+    city, addtional, landmark,
   } = req.body;
   const { id } = req.userData;
 
   try {
     await clientRegistrationSchema.validate(req.body);
 
-    const userExists = await knex("users").where("id", id).first();
-    if (!userExists) return res.status(404).json("O usuário não encontrado.");
+    const clientExists = await knex("clients").where("email", email).orWhere("cpf", cpf).first();
+    if (!clientExists) return res.status(404).json("Cliente já cadastrado.");
 
     const newClient = await knex("clients")
       .insert({ user_id: id, name, email, cpf,
         phone, zipcode, street, state,
-        district, city, additional, landmark,
+        district, city, addtional, landmark,
       }).returning("*");
 
     if (!newClient) return res.status(400).json('Erro ao cadastrar cliente.');
 
-    return res.status(200).json(newClient[0]);
-
+console.log(newClient);
+    return res.status(200).json(newClient);
   } catch (error) {
     return res.status(400).json(error.message);
   }
