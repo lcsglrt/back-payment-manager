8d820cffa719f03f3946b307f4394838a675bd33
Lucas Goulart <goulartlcs@gmail.com>
2021-10-01 20:47:30 -0300

    O campo senha nao e mais obrigatorio

--- src/validations/updateUserProfileSchema.js
+++ src/validations/updateUserProfileSchema.js
@@ -3,7 +3,7 @@ const yup = require('./yup');
 const updateUserProfileSchema = yup.object().shape({
     name: yup.string().required(),
     email: yup.string().email().required(),
-    password: yup.string().required(),
+    password: yup.string(),
     phone: yup.string().nullable(),
     cpf: yup.string().nullable(),
   });
