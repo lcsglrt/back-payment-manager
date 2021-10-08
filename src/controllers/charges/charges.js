6f701986150a87136bafc1d101097e8b61b18e54
Lucas Goulart <goulartlcs@gmail.com>
2021-10-07 23:22:49 -0300

    Salvando cobranca no banco de dados

--- src/controllers/charges/charges.js
+++ src/controllers/charges/charges.js
@@ -1,4 +1,34 @@
+const knex = require('../../database/db');
+const createChargeSchema = require('../../validations/createChargeSchema');
+
 const createCharge = async (req, res) => {
+  const { client_id, description, status, amount, due_date } = req. body;
+  const { id } = req.userData;
+
+  try {
+    console.log(req.body);
+    await createChargeSchema.validate(req.body);
+
+    const now = new Date();
+    
+    const newCharge = await knex('charges').insert({
+      create_at: now,
+      user_id: id,
+      client_id,
+      description,
+      status,
+      amount,
+      due_date
+    }).returning('*');
+
+    console.log(newCharge);
+
+    if (!newCharge) return res.status(400).json('Erro ao criar cobrança.');
+
+    return res.status(200).json(newCharge);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 
 }
 
---
+++ src/validations/createChargeSchema.js
@@ -0,0 +1,12 @@
+const yup = require('./yup');
+
+const createChargeSchema = yup.object().shape({
+    client_id: yup.number().required(),
+    description: yup.string().required(),
+    status: yup.boolean().required(),
+    amount: yup.number().required(),
+    due_date: yup.date().required()
+  });
+
+module.exports = createChargeSchema;
+;
